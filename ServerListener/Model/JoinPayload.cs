fd643571843f7715b9280041c121227f9ae7e121
proxytype <shlomiruder@gmail.com>
2020-08-15 01:44:54 +0300

    Add files via upload

---
+++ ServerListener/Model/JoinPayload.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ServerListener.Model
+{
+    public class JoinPayload
+    {
+        public string name { get; set; }
+        public string sessionID { get; set; }
+        public string ownerID { get; set; }
+    }
+}
---
+++ ServerListener/Model/chatPayload.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ServerListener.Model
+{
+    public class chatPayload
+    {
+        public string ownerID { get; set; }
+        public string name { get; set; }
+        public string sessionID { get; set; }
+        public string message { get; set; }
+    }
+}
